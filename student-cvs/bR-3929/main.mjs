1d08dfda631ca7a05b3a36ede5a2c210dd2c731a
Brian Diaz <beerre20@gmail.com>
2023-04-03 13:36:29 -0300

    Implementando la funcion Humanize al CV

--- student-cvs/bR-3929/main.mjs
+++ student-cvs/bR-3929/main.mjs
@@ -4,35 +4,32 @@ dayjs.extend(relativeTime);
 import es from "dayjs/locale/es.js";
 dayjs.locale(es);
 
-
- function handleSubmit (event){
-    event.preventDefault();
-    console.log("Hola mundo");
+    function humanize(dates){
+        const now = dayjs()
+    
+        if(now.diff(dates, "year") < 1 && now.diff(dates, "month") >= 1){
+            return dayjs().format("MMMM DD");
+        }
+        else if(now.diff(dates, "year") > 1){
+            return dayjs().format("MMMM DD, YYYY");
+        }
+        else{
+        return "Publicado " + now.to(dates);
+        }
     }
     
-    const $form = document.querySelector("form");
-    $form.addEventListener("submit", handleSubmit);
+    const fechaElement = document.getElementById("fechas");
+    const fecha = dayjs("2023-04-01");
+    const fechaHumana = humanize(fecha);
+    fechaElement.innerHTML = fechaHumana;
 
+    
 
-
-
-function humanize(dates){
-    const now = dayjs()
-
-    if(now.diff(dates, "year") < 1 && now.diff(dates, "month") >= 1){
-        return "Publicado " + dayjs().format("MMMM DD");
-    }
-    else if(now.diff(dates, "year") > 1){
-        return "Publicado " + dayjs().format("MMMM DD, YYYY");
-    }
-    else{
-    return "Publicado " + now.to(dates);
-    }
-}
-
-const fechaElement = document.getElementById("fecha");
-const fecha = dayjs("2023-04-01T16:32:00");
-const fechaHumana = humanize(fecha);
-fechaElement.innerHTML = fechaHumana;
-
-console.log(fecha);
+    function handleSubmit (event){
+        event.preventDefault();
+        console.log("Hola mundo");
+        }
+        
+        const $form = document.querySelector("form");
+        $form.addEventListener("submit", handleSubmit);
+    
