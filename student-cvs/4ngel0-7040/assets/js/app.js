fa34bc5ffd88ee329197a0516cd547691ec301c5
Angelo <angelop.casapaico@gmail.com>
2023-04-08 15:02:21 -0500

    Utilizando sass en el cvs

---
+++ student-cvs/4ngel0-7040/assets/js/app.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+    eventListeners();
+});
+
+const eventListeners = () => {
+    const Menu = document.querySelector(".header-barra");
+
+    Menu.addEventListener('click', navegacionResponsive);
+    //Mustra campos condicionales
+    const metodoContacto = document.querySelectorAll('input[name="rd"]');
+
+    metodoContacto.forEach(input => {
+
+        input.addEventListener('click', mostrarMetodoContacto);
+    });
+}
+
+const navegacionResponsive = () => {
+    const navegacion = document.querySelector(".header-navegacion");
+    navegacion.classList.toggle('ocultar');
+}
+
+const mostrarMetodoContacto = (e) => {
+    const contactoDiv = document.querySelector('#contacto');
+
+    if (e.target.value === 'telefono') {
+        contactoDiv.innerHTML = `
+        <div class="form-group"> 
+            <label for="telefono">Número de Teléfono</label>
+            <input type="tel" name="telefono" id="telefono" placeholder="Tu Teléfono" required>
+        </div>
+        `;
+        return;
+    } else {
+        contactoDiv.innerHTML = `
+        <div class="form-group"> 
+            <label for="email">E-mail</label>
+            <input type="email" name="email" id="email" placeholder="Tu E-mail" required>
+        </div>
+        `;
+    }
+}
