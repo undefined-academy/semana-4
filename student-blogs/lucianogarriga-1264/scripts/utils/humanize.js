8529d5800a6bda72e5ddc407e083f995636d500f
luciano <lucianomgarriga@gmail.com>
2023-04-16 11:43:42 -0300

    Add scss and js scripts

--- student-blogs/lucianogarriga-1264/scripts/utils/humanize.js
+++ student-blogs/lucianogarriga-1264/scripts/utils/humanize.js
@@ -1,15 +1,14 @@
+//el import de dayjs viene de node_modules
+//import from "../node_modules/dayjs";
 import dayjs from "dayjs";  
 import relativeTime from "dayjs/plugin/relativeTime.js"
-
-await import("dayjs/locale/es-us.js");
-
-dayjs.locale("es-us")
+ 
 dayjs.extend(relativeTime)
 // para poder usar "fromNow"
 
-console.log(dayjs().format("YYYY-MM-DD"));
+console.log("Hello world from humanize.js"); 
 
-function humanize(date){
+export default function humanize(date){
     const wrappedDate = dayjs(date)
     const daysDiff = Math.abs(wrappedDate.diff(Date.now(), "days"));
 
@@ -23,9 +22,5 @@ function humanize(date){
         return wrappedDate.format("MMMM DD")
     }
 }
-console.log(humanize(Date.now()));//a few seconds ago
-console.log(humanize(dayjs().subtract(5, "days")));//5 days ago
-console.log(humanize(dayjs().subtract(60, "days")));//January 26
-console.log(humanize(dayjs().subtract(500, "days")));//November, 12 - 2021
-console.log(humanize(dayjs().add(30, "days")));//en un mes
-console.log(humanize(dayjs().add(300, "days")));//January, 21 - 2024
+
+
