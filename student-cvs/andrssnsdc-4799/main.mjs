eaa6d637c5d10a63fe4073a1eefe01266e9d283f
Andersson Delgado <anderssonstiven04@gmail.com>
2023-04-19 11:00:34 -0500

    se agrego funcion humanizar fechas

--- student-cvs/andrssnsdc-4799/main.mjs
+++ student-cvs/andrssnsdc-4799/main.mjs
@@ -4,5 +4,21 @@ function handleSubmit(event) {
    }
    const $form =
    document.querySelector("form");
-   $form.addEventListener("submit",
-   handleSubmit);
+   if ($form) {
+    $form.addEventListener("submit", handleSubmit);
+  } else {
+    console.log("No se encontró un formulario en la página");
+  }
+  
+
+   import humanizarFecha from "./script/humanizar_fechas";
+   function humanizarFechas($dates, i) {
+    const datetime = $dates.getAttribute("datetime");
+    const humanizedDate = humanizarFecha(datetime);
+  
+    const fechaEscrita = `Publicado  ${humanizedDate}`;
+    const contenidoAntiguo = $dates.textContent;
+    $dates.textContent = `${fechaEscrita}`;
+  }
+   const $dates = document.querySelectorAll("time");
+   $dates.forEach(humanizarFechas);
