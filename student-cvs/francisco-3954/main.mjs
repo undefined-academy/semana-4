6dc119b77cb9c09559ee6f699efed5a56364789a
noe <martinezruiznoefrancisco@gmail.com>
2023-04-24 17:02:43 -0600

    utilizacion de sass para hacer el css y dayjs para hacer una fecha mas agradable de leer

---
+++ student-cvs/francisco-3954/main.mjs
@@ -0,0 +1,44 @@
+import dayjs from "dayjs";
+import "dayjs/locale/es.js"
+import relativeTime from "dayjs/plugin/relativeTime.js";
+
+dayjs.locale('es')
+dayjs.extend(relativeTime)
+
+function humanizarFecha(date) {
+
+    const dateFrom = dayjs(date)
+    const today = dayjs()
+    
+    const years = today.diff(dateFrom, "years")
+    const months = today.diff(dateFrom, "months")
+
+    
+    if(years) {
+        return dateFrom.format("MMMM DD, YYYY")
+    } else if(months > 1) {
+        return dateFrom.format("MMMM DD")
+    } else {
+        return dateFrom.fromNow()
+    }
+
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+}
+    
+const $form =document.querySelector("form");
+$form.addEventListener("submit", handleSubmit);
+
+const dates = document.getElementsByTagName("time")
+
+for (let index = 0; index < dates.length; index++) {
+    const element = dates[index];
+    
+    const humanizateDate = humanizarFecha(element.dateTime)
+    
+    element.innerText = humanizateDate
+}
+
+
