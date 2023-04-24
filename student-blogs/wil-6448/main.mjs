dd5c4a790191aa34f9f9fee00d2f7ef9dcf75fa5
Wilmer Gulcochia <karlwgs1989@gmail.com>
2023-04-24 18:30:40 -0500

    Se actualizó el código del Blog y se hizo uso de las variables y módulos con SAS

---
+++ student-blogs/wil-6448/main.mjs
@@ -0,0 +1,47 @@
+//humanize.js
+
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime.js"
+
+await import("dayjs/locale/es-us.js");
+
+dayjs.locale("es-us");
+dayjs.extend(relativeTime);
+
+// console.log(dayjs().format());
+
+function humanize(date) {
+    const wrappedDate = dayjs(date);
+    const daysDiff = Math.abs(wrappedDate.diff(Date.now(), "days"));
+
+    const isCurrentYear = wrappedDate.year() === new Date().getFullYear();
+
+    if(!isCurrentYear) {
+        return wrappedDate.format("MMM DD YYYY")
+    } else if(daysDiff <= 30) {
+        return wrappedDate.fromNow();
+    } else {
+        return wrappedDate.format("MMM DD");
+    }
+}
+
+function humanizeDate($date) {
+    const datetime = $date.getAttribute("datetime");
+    const humanizedDate = humanize(datetime);
+    $date.textContent = capitalize(humanizedDate); 
+}
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function humanizeArticlesDate() {
+    const $dates = document.querySelectorAll("time");
+    $dates.forEach(humanizeDate);
+}
+
+function main() {
+    humanizeArticlesDate();
+}
+
+main();
