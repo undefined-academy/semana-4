d344f804103176b96e76681b3bf84f2757bd94e9
Wilmer Gulcochia <karlwgs1989@gmail.com>
2023-04-24 20:33:09 -0500

    Se modificó el main.mjs de la carpeta student-blogs/wil-6468.

--- student-blogs/wil-6448/main.mjs
+++ student-blogs/wil-6448/main.mjs
@@ -3,7 +3,8 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime.js"
 
-await import("dayjs/locale/es-us.js");
+//await import("dayjs/locale/es-us.js");
+import("dayjs/locale/es-us.js");
 
 dayjs.locale("es-us");
 dayjs.extend(relativeTime);
