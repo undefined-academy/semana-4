0432a66eb92510061c29c9824c894b0b60ca8e05
Bryan Ortega <brayortepla12@gmail.com>
2023-04-26 09:42:37 -0500

    agg alt in img / using function humanizarFecha

--- student-cvs/Bryan_Ortega_Plata-1009/main.mjs
+++ student-cvs/Bryan_Ortega_Plata-1009/main.mjs
@@ -9,19 +9,21 @@ dayjs.extend(relativeTime)
 dayjs.extend(weekday)
 dayjs.locale("es-us")
 
-const fecha = dayjs("2023-02-30")
 const fechaHoy = dayjs()
 
-const diferenciaMes = fechaHoy.diff(fecha, "month")
 function humanizarFecha(fecha) {
   if (fechaHoy.get("year") != fecha.get("year")) {
     return fecha.format("MMMM, DD YYYY")
-  } else if (diferenciaMes == 0) {
+  } else if (fechaHoy.diff(fecha, "month") == 0) {
     return fecha.fromNow()
-  } else if (diferenciaMes > 0) {
+  } else if (fechaHoy.diff(fecha, "month") > 0) {
     return fecha.format("MMMM DD")
   }
 }
 
-const humanizado = humanizarFecha(fecha)
-console.log(humanizado)
+const element = document.querySelector('time.date');
+const fecha = element.getAttribute('datetime');
+element.innerHTML = humanizarFecha(dayjs(fecha))
+
+
+
