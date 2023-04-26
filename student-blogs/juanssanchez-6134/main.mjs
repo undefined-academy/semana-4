78cf5a4316469a75a6bc6785bd13550d7d6a99e4
Juan <js.sanchez130@gmail.com>
2023-04-25 20:36:56 -0500

    Implementar función de humanizar fechas en blog

--- student-blogs/juanssanchez-6134/main.mjs
+++ student-blogs/juanssanchez-6134/main.mjs
@@ -12,18 +12,24 @@ function humanizeDate(date) {
 
     const isCurrentYear = dayjsDate.year() === new Date().getFullYear()
 
-    
+
     if (!isCurrentYear) {
         return dayjsDate.format("MMMM DD, YYYY")
-    } else if (daysDiff<=30) {
+    } else if (daysDiff <= 30) {
         return dayjsDate.fromNow()
     } else {
         return dayjsDate.format("MMMM DD")
     }
 }
 
-console.log(humanizeDate(Date.now()))
-console.log(humanizeDate(dayjs().subtract(10, "days")))
-console.log(humanizeDate(dayjs().subtract(60, "days")))
-console.log(humanizeDate(dayjs().subtract(500, "days")))
-console.log(humanizeDate(dayjs().add(300, "days")))
+const dates = document.querySelectorAll('.date');
+dates.forEach((date) => {
+    const humanDate = humanizeDate(date.textContent);
+    date.textContent = humanDate;
+});
+
+// console.log(humanizeDate(Date.now()))
+// console.log(humanizeDate(dayjs().subtract(10, "days")))
+// console.log(humanizeDate(dayjs().subtract(60, "days")))
+// console.log(humanizeDate(dayjs().subtract(500, "days")))
+// console.log(humanizeDate(dayjs().add(300, "days")))
