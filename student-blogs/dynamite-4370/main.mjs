b19116dd0e62a2d300165ec88941b76db0539d35
Daniel Lozano <dlozanoc862@gmail.com>
2023-04-30 10:34:44 -0500

    :sparkles: Se utiliza la funcion humanizarFecha

--- student-blogs/dynamite-4370/main.mjs
+++ student-blogs/dynamite-4370/main.mjs
@@ -1,10 +1,33 @@
 import dayjs from "dayjs";
 import { entries } from "./data/post.mjs";
+import relativeTime from "dayjs/plugin/relativeTime.js";
+await import("dayjs/locale/es-us.js")
 
+dayjs.locale("es-us");
+dayjs.extend(relativeTime);
 
+function humanizeDate(date) {
+    const wrappedDate = dayjs(date);
+    const daysDiff = Math.abs(wrappedDate.diff(Date.now(), "days"));
+
+    const isCurrentYear = wrappedDate.year() === new Date().getFullYear();
+
+    if (!isCurrentYear) {
+        return wrappedDate.format("MMMM DD, YYYY");
+    }else if (daysDiff <= 30){
+        return wrappedDate.fromNow();
+    }else{
+        return wrappedDate.format("MMMM DD");
+    }
+}
+
+//Llenar Posts
 const postsContainer = document.querySelector('#posts');
 
 entries.forEach((entry) => {
+
+    const {titulo, descripcion, fecha} =entry;
+
     const post = document.createElement('article');
     post.classList.add('post1');
     post.innerHTML = `
@@ -14,11 +37,11 @@ entries.forEach((entry) => {
         <div class="post-principal">
             <div class="post-readtime">
                 <p>HTLM</p>
-                <span>5min Read</span>
+                <span>${humanizeDate(fecha)}</span>
             </div>
             <div class="post-info">
-                <h3>${entry.titulo}</h3>
-                <p>${entry.descripcion}</p>
+                <h3>${titulo}</h3>
+                <p>${descripcion}</p>
             </div>
             <div class="post-button">
                 <button>Ver mas</button>
