857f397c9264cbc83f70a8cadc3d94e962e907e8
Daniel Lozano <dlozanoc862@gmail.com>
2023-04-30 10:20:07 -0500

    :sparkles: Se agregan los post de forma dinamica

--- student-blogs/dynamite-4370/main.mjs
+++ student-blogs/dynamite-4370/main.mjs
@@ -1,4 +1,30 @@
 import dayjs from "dayjs";
 import { entries } from "./data/post.mjs";
 
-console.log(entries);
+
+const postsContainer = document.querySelector('#posts');
+
+entries.forEach((entry) => {
+    const post = document.createElement('article');
+    post.classList.add('post1');
+    post.innerHTML = `
+        <div class="post-image">
+            <img class="image" alt="imagen" src="https://source.unsplash.com/uppjafUWPj8">
+        </div>
+        <div class="post-principal">
+            <div class="post-readtime">
+                <p>HTLM</p>
+                <span>5min Read</span>
+            </div>
+            <div class="post-info">
+                <h3>${entry.titulo}</h3>
+                <p>${entry.descripcion}</p>
+            </div>
+            <div class="post-button">
+                <button>Ver mas</button>
+            </div>
+        </div>
+    `;
+
+    postsContainer.appendChild(post);
+});
