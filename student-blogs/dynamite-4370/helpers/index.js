724c484ba64f91ba6cdd7a87cbe0e4fac7eaadfa
Daniel Lozano <dlozanoc862@gmail.com>
2023-05-01 11:29:35 -0500

    :bug: Removiendo promesa top-level en helpers

--- student-blogs/dynamite-4370/helpers/index.js
+++ student-blogs/dynamite-4370/helpers/index.js
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime.js";
-await import("dayjs/locale/es-us.js");
+import("dayjs/locale/es-us.js");
 dayjs.locale("es-us");
 dayjs.extend(relativeTime);
 
