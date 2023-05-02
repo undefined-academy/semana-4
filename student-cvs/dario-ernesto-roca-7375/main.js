03c1f5dc815879df83756394a726c0976ed7cf6d
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2023-05-01 20:17:23 -0500

    Agrega función humanizar a las fechas del Blog

---
+++ student-cvs/dario-ernesto-roca-7375/main.js
@@ -0,0 +1,12 @@
+// Function humanize
+
+
+
+// main.mjs
+function handleSubmit(event) {
+    event.preventDefault(); // 🍎
+    console.log("Hola mundo");
+}
+    
+const $form =document.querySelector("form");
+$form.addEventListener("submit", handleSubmit);
