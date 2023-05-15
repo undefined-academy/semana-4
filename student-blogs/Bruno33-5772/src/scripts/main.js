af9cc9acd152178ca33d2c534c8b1839909fb758
bmonge <brunom03@hotmail.com>
2023-05-15 16:21:18 +0200

    Archivo js para conversión de fechas.

--- student-blogs/Bruno33-5772/src/scripts/main.js
+++ student-blogs/Bruno33-5772/src/scripts/main.js
@@ -1,14 +1,17 @@
 import humanizeDate from "./utils/humanize.js";
 
+// Fecha con mayúscula al inicio
 function capitalize (str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+// Por cada time, convertir el datetime en fecha más fácil de leer
 function humanizeDatePost ($date) {
     const datetime = $date.getAttribute("datetime");
     const humanizedDate = humanizeDate(datetime);
     $date.textContent = capitalize(humanizedDate);
 }
 
+// Obtener todos los elementos time para conversión
 const $dates = document.querySelectorAll("time");
 $dates.forEach(humanizeDatePost);
