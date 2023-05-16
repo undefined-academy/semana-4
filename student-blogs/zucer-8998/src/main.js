80ad4b18dfb2247dab94cf1075c739b9f5333551
Julian Sucerquia <andresucerquia@gmail.com>
2023-05-16 01:31:25 -0500

    feat: add comments in humanize function

--- student-blogs/zucer-8998/src/main.js
+++ student-blogs/zucer-8998/src/main.js
@@ -1,4 +1,3 @@
-
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime.js";
 
@@ -7,24 +6,29 @@ await import("dayjs/locale/es-us.js");
 dayjs.locale("es-us");
 dayjs.extend(relativeTime);
 
+/**
+ * Make given date comprehensive
+ * @param {string} date
+ * @returns string
+ */
 function humanize(date) {
-  const wrappedDate = dayjs(date)
+  const wrappedDate = dayjs(date);
   const daysDiff = Math.abs(wrappedDate.diff(Date.now(), "days"));
 
   const isCurrentYear = wrappedDate.year() === new Date().getFullYear();
 
-  if(!isCurrentYear) {
+  if (!isCurrentYear) {
     return wrappedDate.format("MMMM DD, YYYY");
-  } else if(daysDiff <=30) {
+  } else if (daysDiff <= 30) {
     return wrappedDate.fromNow();
   } else {
     return wrappedDate.format("MMMM DD");
   }
 }
 
-function setHumanDate (elementId){
-    let lastUpdateDate = document.getElementById(elementId);
-    lastUpdateDate.innerHTML = humanize(lastUpdateDate.innerHTML);
+function setHumanDate(elementId) {
+  let lastUpdateDate = document.getElementById(elementId);
+  lastUpdateDate.innerHTML = humanize(lastUpdateDate.innerHTML);
 }
-    
+
 setHumanDate("lastUpdate");
