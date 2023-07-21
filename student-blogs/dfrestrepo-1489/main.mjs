46347e4eb9e4c33d321a0300ce9e898c5161ac97
Daniel Restrepo <danielcgrafico@email.com>
2023-07-21 16:53:59 +0200

    Fix humanizar fechas de varios tipos

--- student-blogs/dfrestrepo-1489/main.mjs
+++ student-blogs/dfrestrepo-1489/main.mjs
@@ -1,32 +1,37 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime.js";
+import customParseFormat from "dayjs/plugin/customParseFormat"
 
 await import("dayjs/locale/en-au.js")
 
 dayjs.locale("en-au")
 dayjs.extend(relativeTime)
+dayjs.extend(customParseFormat)
 
 function toHumanize(date) {
-    const wrappedDate = dayjs(date)
+    const wrappedDate = dayjs(date, "DD-MM-YYYY")
     const daysDiff = Math.abs(wrappedDate.diff(Date.now(), "days"));
 
-    const isCurrentYear = wrappedDate.year() === new Date().getFullYear();
+	const isCurrentYear = wrappedDate.year() === new Date().getFullYear();
 
-    if(!isCurrentYear) {
-        return wrappedDate.format("MMMM DD, YYYY")
-    } else if(daysDiff <= 30) {
-        return wrappedDate.fromNow();
-    } else {
-        return wrappedDate.format("MMMM DD")
-    }
+	if(!isCurrentYear) {
+		return wrappedDate.format("MMMM DD, YYYY")
+	} else if(daysDiff <= 30) {
+		return wrappedDate.fromNow();
+	} else {
+		return wrappedDate.format("MMMM DD")
+	}
 }
 
 
-const toPrint = toHumanize(dayjs().subtract(365, "days"))
+  const dates = document.querySelectorAll("time")
+    dates.forEach(function(date) {
+    
+	const datetime = date.getAttribute("datetime")
 
+    console.log("datetime: " + datetime)
 
-var cardDate = document.getElementsByClassName("card-date");
-
-for (var i = 0; i < cardDate.length; i++) {
-  cardDate[i].innerHTML = toPrint;
-  }
+	const transformDate = toHumanize(datetime);
+	console.log(transformDate)
+	date.textContent = transformDate
+})
