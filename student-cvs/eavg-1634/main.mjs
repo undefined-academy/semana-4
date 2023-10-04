f9f61f6e364b37f563619047fe5ccc38a586581d
Esmelin Vivanco <eavivancog@gmail.com>
2023-10-04 10:04:36 -0500

    add: function humanize students-cvs

--- student-cvs/eavg-1634/main.mjs
+++ student-cvs/eavg-1634/main.mjs
@@ -1,3 +1,25 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+await import("dayjs/locale/es");
+
+dayjs.locale("es");
+dayjs.extend(relativeTime);
+
+function humanize(date) {
+  const wrappedDate = dayjs(date);
+  const diffDate = Math.abs(wrappedDate.diff(dayjs(), "days"));
+  const today = dayjs();
+
+  if (wrappedDate.year() !== today.year()) {
+    return wrappedDate.format("MMMM DD");
+  } else if (diffDate <= 30) {
+    return wrappedDate.fromNow();
+  } else {
+    return wrappedDate.format("MMMM DD, YYYY");
+  }
+}
+
 function handleSubmit(event) {
   event.preventDefault(); //
   console.log("Hola mundo");
